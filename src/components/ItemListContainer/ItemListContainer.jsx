f67fdd3ef2daafa9a8a5fd962877d568d0c5db88
SantiagoRibot96 <85650377+santiagoribot@users.noreply.github.com>
2023-10-24 14:22:53 -0300

    preEntrega2

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { getProductos, getUnaCategoria } from "../../asyncmock";
+import ItemList from "../ItemList/ItemList";
+import { useParams } from "react-router-dom";
+import './ItemListContainer.css'
 
-const ItemListContainer = (props) => {
+const ItemListContainer = () => {
+    const [productos, setProductos] = useState([]);
 
-  const degrade = {
-    background: "linear-gradient(to right, red 25%, yellow, blue, orange)",
-    color: "blue",
-    border: "solid black",
-    padding: "1rem"
-  };
+    const {idCategoria} = useParams();
 
-  return (
-    <h2 style={degrade}> {props.greeting} </h2>
-  )
+    useEffect(() => {
+        const funcionProductos = idCategoria ? getUnaCategoria : getProductos;
+
+        funcionProductos(idCategoria)
+            .then(res => setProductos(res))
+            .catch(error => console.log(error));
+    }, [idCategoria])
+
+    return (
+        <>
+            <h2 className="h2-destacados">Productos destacados</h2>
+            <ItemList productos={productos} />
+        </>
+    )
 }
 
 export default ItemListContainer
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,32 +1,23 @@
-import React from 'react'
-import CartWidget from '../CartWidget/CartWidget'
+import CartWidget from '../CartWidget/CartWidget';
 import './Navbar.css';
+import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <header>
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
-                <button className="nav-link navbar-brand" href="#">TodoParrillas</button>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse centrado" id="navbarNav">
+                <Link className="nav-link navbar-brand fuenteGrande" to="/">Todo Parrillas</Link>
+                <div className="centrado" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                        <button className="nav-link">Inicio</button>
+                            <NavLink className="nav-link navbar-brand" to="categoria/carbon">Carbon</NavLink>
                         </li>
                         <li className="nav-item">
-                        <button className="nav-link">Parrillas</button>
+                            <NavLink className="nav-link navbar-brand" to="categoria/gas">Gas</NavLink>
                         </li>
                         <li className="nav-item">
-                        <button className="nav-link">Contacto</button>
-                        </li>
-                        <li className="nav-item">
-                        <button className="nav-link">Donde nos ubicamos?</button>
-                        </li>
-                        <li className="nav-item">
-                        <button className="nav-link"><CartWidget/></button>
+                            <NavLink className="nav-link"> <CartWidget/> </NavLink>
                         </li>
                     </ul>
                 </div>
