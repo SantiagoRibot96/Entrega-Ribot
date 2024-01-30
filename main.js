54f8c79f3958db4f32da9c2cc59e88c902d0e33d
SantiagoRibot96 <85650377+santiagoribot@users.noreply.github.com>
2024-01-30 15:58:56 -0300

    Desafio 1

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /* 
 ProductManager es una clase que cuenta con tres metodos:
     -getProducts(): Obtiene los productos existentes;
-    -addProduct(titulo, descripcion, precio, url, codigo, stock): Agrega un nuevo producto con los parametros pasados. En caso de que falte alguno, arroja error. En caso que el codigo se repita con otro producto, tambien arroja error. A cada producto le asigna un Id correlativo. Los errores son retornados, deben ser mostrados por linea de consola si asi lo desea;
+    -addProduct(titulo, descripcion, precio, url, codigo, stock): Agrega un nuevo producto con los parametros pasados. En caso de que falte alguno, arroja error. En caso que el codigo se repita con otro producto, tambien arroja error. A cada producto le asigna un Id correlativo. Retorna true o false dependiendo si pudo ejecutar la operacion;
     -getProducts(id): Busca dentro de los productos existentes uno con el Id solicitado. En caso de encontrarlo retorna el producto, en caso contrario retorna un error.
 
 Al utilizar el constructor de ProductManager se crea un array vacio de productos.
