ffa904bccefa0b03fc74e2d0b5c1b2fed8517b9c
SantiagoRibot96 <85650377+santiagoribot@users.noreply.github.com>
2024-02-10 19:17:53 -0300

    Desafio 3

---
+++ src/app.js
@@ -0,0 +1,42 @@
+/* 
+
+*/
+
+import { ProductManager } from "./class.js";
+import { PORT, app, createProducts} from "./init.js"
+
+let newProductList = new ProductManager("./productos.json");
+
+createProducts(newProductList);
+
+app.get("/", (req, res) => {
+    res.send(`Desafio N°3 de CoderHouse-Backend. Con /products se te mostrara todos los productos en la lista. Con /products?limit=X se te mostraran solo X cantidad de productos. Con /products/:pid se te mostrara el producto correspondiente al pid solicitado. Por ejemplo /products/4`);
+});
+
+app.get("/products", async (req, res) => {
+    const limit = parseInt(req.query.limit);
+
+    const myProducts = await newProductList.getProducts();
+
+    if(limit >= 1) {
+        res.send(myProducts.slice(0, limit));
+    }else {
+        res.send(myProducts);
+    }
+});
+
+app.get("/products/:pid", async (req, res) => {
+    const pid = parseInt(req.params.pid);
+    
+    const product = await newProductList.getProductById(pid);
+
+    if(product){
+        res.send(product);
+    }else{
+        res.send({status: "failed", message: "Producto no encontrado"})
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Abri el navegador en http://localhost:${PORT}`);
+});
